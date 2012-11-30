9dfa3235ca5985cbc782f789b647ba21c766d21e
asndav <asndav@gmail.com>
2012-11-30 17:16:14 +0100

    Mejoras en el dialogo de info y creditos

--- agricola2P-contador/src/es/agricola/android/OptionsMenuActivity.java
+++ agricola2P-contador/src/es/agricola/android/OptionsMenuActivity.java
@@ -3,10 +3,14 @@ package es.agricola.android;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class OptionsMenuActivity extends Activity {
 
@@ -22,10 +26,10 @@ public class OptionsMenuActivity extends Activity {
 		Dialog dialog = null;
 		switch (id) {
 		case R.id.creditos:
-			dialog = buildDialogo(R.string.detalle_creditos);
+			dialog = buildDialogo(R.string.detalle_creditos, R.string.creditos);
 			break;
 		case R.id.instrucciones:
-			dialog = buildDialogo(R.string.detalle_instrucciones);
+			dialog = buildDialogo(R.string.detalle_instrucciones, R.string.instrucciones);
 			break;
 		default:
 			dialog = null;
@@ -45,9 +49,17 @@ public class OptionsMenuActivity extends Activity {
 		}
 	}
 
-	protected Dialog buildDialogo(int idTexto) {
+	private Dialog buildDialogo(int idTexto, int idTitulo) {
+		Context mContext = getApplicationContext();
+		LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+		View layout = inflater.inflate(R.layout.info, null);
+
+		TextView helpText = (TextView) layout.findViewById(R.id.TextView_HelpText);
+        helpText.setText(getResources().getString(idTexto));
+
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setMessage(idTexto)
+		builder.setView(layout)
+				.setTitle(idTitulo)
 				.setCancelable(true)
 				.setPositiveButton(R.string.aceptar,
 						new DialogInterface.OnClickListener() {
